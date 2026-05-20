87b5ef9e17e3ffba326bab7fc182d7810bef8b7b
Mullois Matthéo(cat_frozen) <mattheoducret86@gmail.com>
2026-05-20 22:53:38 +0200

    [content] "Ajoute les liens utiles de l'accueil"

--- MVC/vues/pages/accueil.php
+++ MVC/vues/pages/accueil.php
@@ -150,6 +150,31 @@ $dammierPayload = [
         <div class="section-head section-head--compact">
             <h2>Liste de liens utiles</h2>
         </div>
+
+        <ul class="bullet-list useful-links-list">
+            <li>
+                <a
+                    class="useful-link"
+                    href="https://www.echecs.asso.fr/ListeJoueurs.aspx?Action=JOUEURCLUBREF&amp;ClubRef=3012"
+                    target="_blank"
+                    rel="noopener noreferrer external"
+                    referrerpolicy="no-referrer"
+                >
+                    Fédération Française D'Échecs(FFE)
+                </a>
+            </li>
+            <li>
+                <a
+                    class="useful-link"
+                    href="https://www.normandie-echecs.fr/cdje14"
+                    target="_blank"
+                    rel="noopener noreferrer external"
+                    referrerpolicy="no-referrer"
+                >
+                    Ligue de Normandie des échecs
+                </a>
+            </li>
+        </ul>
     </article>
 </section>
 
